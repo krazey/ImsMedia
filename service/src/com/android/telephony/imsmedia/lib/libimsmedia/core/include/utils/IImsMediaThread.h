d83a78f8fabea65346087e2a51b26eba1a848b04
bodamnam <bodamnam@google.com>
2023-07-07 23:49:10 +0000

    Increase the priority of player and source threads.
    Increase in thead priority will ensure AOC and JitterBuffer calls are made consistent every 20msecs.
    
    Bug: 306298801, 289472217
    Test: adb shell 'ps -Tl -p $(pgrep imsmedia)'
    Change-Id: Ie7c49b88aeebee72091db676f30cc71342c06064

--- service/src/com/android/telephony/imsmedia/lib/libimsmedia/core/include/utils/IImsMediaThread.h
+++ service/src/com/android/telephony/imsmedia/lib/libimsmedia/core/include/utils/IImsMediaThread.h
@@ -21,6 +21,9 @@
 
 #define MAX_EVENTHANDLER_NAME 256
 
+// Thread priority value used with SCH_FIFO scheduling policy to set Real-Time priority.
+#define THREAD_PRIORITY_REALTIME 2
+
 /**
  * @class IImsMediaThread
  * @brief Base class of thread
@@ -33,7 +36,7 @@ public:
     IImsMediaThread();
     virtual ~IImsMediaThread();
     bool StartThread();
-    void SetAudioThreadPriority(pid_t tid);
+    void SetThreadPriority(pid_t pid, pid_t tid, int priority);
     void StopThread();
     bool IsThreadStopped();
     void* runBase();
