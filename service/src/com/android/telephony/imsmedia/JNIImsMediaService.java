9ddb8b6bc8faaf3dbd9b737dc5da5a3428913ff3
Jared Duke <jdduke@google.com>
2024-08-09 22:39:17 +0000

    Enable shrinking for ImsMediaService
    Include the necessary keep annotations to preserve JNI code, and enable
    basic shrinking of ImsMediaService. No service-specific classes
    should be removed with this change.
    
    Bug: 343759652
    Test: presubmit + apkdiffstats
    Flag: EXEMPT bugfix
    Change-Id: I7a7f4b1394263deb4a7cf44642b829f0c75e9978

--- service/src/com/android/telephony/imsmedia/JNIImsMediaService.java
+++ service/src/com/android/telephony/imsmedia/JNIImsMediaService.java
@@ -25,8 +25,14 @@ import android.view.Surface;
 import androidx.annotation.VisibleForTesting;
 
 import com.android.telephony.imsmedia.util.Log;
+import com.android.tools.r8.keepanno.annotations.KeepItemKind;
+import com.android.tools.r8.keepanno.annotations.UsedByNative;
 
 /** JNI interface class to send message to libimsmediajni */
+@UsedByNative(
+        description = "Called from JNI in jni/libimsmediajni.cpp",
+        kind = KeepItemKind.CLASS_AND_MEMBERS
+)
 public class JNIImsMediaService {
     private static final String TAG = "JNIImsMediaService";
     private static final int THREAD_PRIORITY_REALTIME = -20;
@@ -163,6 +169,7 @@ public class JNIImsMediaService {
      * @param baData byte array form of data to send
      * @return 1 if it is success to send data, -1 when it fails
      */
+    @UsedByNative
     public static int sendData2Java(final int sessionId, final byte[] baData) {
         Log.dc(TAG, "sendData2Java() - sessionId=" + sessionId);
         JNIImsMediaListener listener = getListener(sessionId);
@@ -188,6 +195,7 @@ public class JNIImsMediaService {
      *
      * @param threadId is the id of the thread whose priority should to be increased.
      */
+    @UsedByNative
     public static void setAudioThreadPriority(int threadId) {
         Log.d(TAG, "setAudioThreadPriority. tid:" + threadId);
         Process.setThreadPriority(threadId, THREAD_PRIORITY_REALTIME);
